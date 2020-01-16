c8ad471b818e1afc6d06daac5268ecc7029a4164
Halim <dahalim17@gmail.com>
2020-01-16 10:29:39 +0700

    second commit

--- AddTwoIntegers.java
+++ AddTwoIntegers.java
@@ -2,7 +2,7 @@ public class AddTwoIntegers {
     public static void main(String[] args) {
         
         int first = 30;
-        int second = 20;
+        int second = 30;
         int sum = first + second;
         System.out.println("The sum is: " + sum);
     }
